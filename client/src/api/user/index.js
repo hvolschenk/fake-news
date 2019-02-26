8638910670a43f6291b2dc897082cafc4c01d7e6
Hendrik Volschenk <righteous.trespasser@gmail.com>
2019-02-26 23:59:41 +0200

    fix: move restart api call to correct folder

--- client/src/api/user/index.js
+++ client/src/api/user/index.js
@@ -1,3 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as getUser } from './get';
-export { default as restartUser } from './post';
+export { default as restartUser } from './restart';
